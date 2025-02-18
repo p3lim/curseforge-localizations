8ceecd2fdf32553b630b3be7bf06ec6dea687581
Adrian L Lange <git@p3lim.net>
2025-02-18 05:09:56 +0100

    Rewrite in python
    This allows _way_ more flexibility, especially with regex patterns.
    
    Also shifted away from using environment variables (although still
    supported for the API key because it's most convenient in CI) to CLI
    arguments, as it's easier to deal with in python. The action has been
    updated to reflect this

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CurseForge Localizations
 
-A small shell script to manage localization strings for World of Warcraft AddOns when using [CurseForge's localization feature](https://support.curseforge.com/en/support/solutions/articles/9000197356-project-localization).
+A small script to manage localization strings for World of Warcraft AddOns when using [CurseForge's localization feature](https://support.curseforge.com/en/support/solutions/articles/9000197356-project-localization).
 
 It will (recursively) scan Lua files for text matching the [AceLocale](https://www.wowace.com/projects/ace3/pages/api/ace-locale-3-0) format (e.g. `L["An example"]`), then upload them to CurseForge. In essence it'll keep the strings used in the project code in-sync with CurseForge. It's a perfect companion to [localization substitution](https://github.com/BigWigsMods/packager/wiki/Localization-Substitution) in packagers.
 
@@ -10,24 +10,37 @@ The script uses the [CurseForge Upload API](https://support.curseforge.com/en/su
 
 ## Usage
 
-For it to work it needs two environment variables:
+```
+usage: update.py [-k KEY] [-i ID] [-m OPT] [-n OPT] [-h]
 
-- `CF_API_KEY` - generate one here: <https://authors-old.curseforge.com/account/api-tokens>
-- `CF_PROJECT_ID` - see the right column of your project's webpage on CurseForge
-  - if not provided the script will attempt to scan TOC files for the `X-Curse-Project-ID` field and use its value
+required arguments::
+  -k, --key KEY        API key or path to file that contains the key for CurseForge,
+                       can also be defined as CF_API_KEY environment variable
+  -i, --id ID          project ID on CurseForge
+
+optional arguments::
+  -m, --missing OPT    how to handle missing phrases (default = DoNothing)
+  -n, --namespace OPT  namespace to upload to
+  -h, --help           show this help message
+```
+
+For the tool to work correctly (i.e. upload strings) it needs at minimum two pieces of information:
+
+- it needs the API key to talk to the CurseForge API
+  - you can generate one here: <https://authors-old.curseforge.com/account/api-tokens>
+  - the script can read this from a file, an environment variable, or as a plain string argument
+- it needs the project ID on CurseForge
+  - see the right column of your project's webpage on CurseForge
+  - if this is not provided as an argument the script will attempt to find it from the `X-Curse-Project-ID` field in nearby TOC files, as it's commonly used/defined that was by other tools
 
 And you obviously need to enable the Localization feature on CurseForge for your project;
 
 > `https://legacy.curseforge.com/wow/addons/YOUR_PROJECT/settings/general`
 
-Additional environment variables that adjust how it runs:
+The additional options should be pretty straight-forward, although some are quite advanced.  
+Please refer to the CurseForge support article for some of them.
 
-- `HANDLE_MISSING` - when an existing string on CurseForge doesn't exist in the Lua files, one of:
-  - `DoNothing` (default)
-  - `DeletePhrase` (recommended to keep things clean)
-  - `DeleteIfTranslationsOnlyExistForSelectedLanguage`
-  - `DeleteIfNoTranslations`
-- `NAMESPACE` - (advanced) if you're using this then you know what you're doing
+If you have any questions on how to use the script, feel free to open a [discussion](https://github.com/p3lim/curseforge-localizations/discussions).
 
 ## Example workflow
 
@@ -52,7 +65,8 @@ jobs:
 
       - name: Upload localizations
         uses: p3lim/curseforge-localizations@v1
+        with:
+          handle_missing: DeletePhrase # optional
         env:
           CF_API_KEY: ${{ secrets.CF_API_KEY }}
-          HANDLE_MISSING: DeletePhrase # optional
 ```
---
+++ update.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+import json
+import glob
+import argparse
+import textwrap
+import requests
+
+url = 'https://legacy.curseforge.com/api/projects/%s/localization/import'
+valid_handlers = ('DoNothing', 'DeletePhrase', 'DeleteIfTranslationsOnlyExistForSelectedLanguage', 'DeleteIfNoTranslations')
+pattern = re.compile(r'L\[["\']([^]]+)["\']\](?:\s*=\s*["\']([^"\']+)["\'])?')
+
+def parse_arguments():
+  parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
+
+  required = parser.add_argument_group('required arguments:')
+  required.add_argument('-k', '--key', help='API key or path to file that contains the key for CurseForge,\ncan also be defined as CF_API_KEY environment variable')
+  required.add_argument('-i', '--id', help='project ID on CurseForge')
+
+  optional = parser.add_argument_group('optional arguments:')
+  optional.add_argument('-m', '--missing', help=f'how to handle missing phrases (default = {valid_handlers[0]})', default=valid_handlers[0], metavar='OPT')
+  optional.add_argument('-n', '--namespace', help='namespace to upload to', metavar='OPT')
+  optional.add_argument('-h', '--help', help='show this help message', action='store_true')
+
+  args = parser.parse_args()
+  if args.help:
+    parser.print_help()
+    sys.exit(0)
+
+  return args
+
+def validate_arguments(args):
+  if not args.key:
+    if 'CF_API_KEY' in os.environ:
+      args.key = os.environ['CF_API_KEY']
+    else:
+      print('error: missing API key')
+      sys.exit(1)
+  else:
+    if os.path.isfile(args.key):
+      with open(args.key, 'r') as file:
+        args.key = file.readline().rstrip()
+
+  if not args.id:
+    toc_pattern = re.compile(r'## X-Curse-Project-ID: (.+)')
+    for path in glob.glob('**/*.toc', recursive=True):
+      with open(path, 'r') as file:
+        for line in file:
+          match = toc_pattern.search(line)
+          if match:
+            args.id = match.group(1)
+            break
+  if not args.id:
+    print('error: missing project ID')
+    sys.exit(1)
+
+def get_metadata(args):
+  metadata = {'language': 'enUS'}
+  if args.namespace:
+    metadata['namespace'] = args.namespace
+
+  if args.missing in valid_handlers:
+    metadata['missing-phrase-handling'] = args.missing
+  else:
+    print('error: invalid missing phrase handler')
+    print(f'       must be one of: {valid_handlers}') # TODO: pretty up this output
+    sys.exit(1)
+
+  return metadata
+
+def get_strings(args):
+  strings = {}
+  for path in glob.glob('**/*.lua', recursive=True):
+    with open(path, 'r') as file:
+      for line in file:
+        match = pattern.search(line)
+        if match:
+          (key, value) = match.groups()
+
+          # if there's no value store "True" in its place, as the AceLocale format CurseForge follows
+          # will interpret that as "the key is the value", but if we find the value for the key then
+          # store that instead
+          if key in strings:
+            if strings[key] is True:
+              strings[key] = value or True
+          else:
+            strings[key] = value or True
+
+  return strings
+
+def upload_strings(args, strings):
+  res = requests.post(
+    url % args.id,
+    headers = {
+      'X-Api-Token': args.key
+    },
+    files = {
+      'metadata': (None, json.dumps(get_metadata(args))),
+      'localizations': (None, '\n'.join([f'L[{json.dumps(k)}] = {json.dumps(v)}' for k,v in strings.items()]))
+    }
+  )
+
+  if res.status_code == 403:
+    # the API doesn't know how to handle this error
+    print('error: failed to upload')
+    print('       this is usually caused by a bad project ID')
+    sys.exit(1)
+
+  data = json.loads(res.content)
+  if res.status_code == 200:
+    print(data["message"])
+  else:
+    # do some trimming on the message
+    msg = data["errorMessage"].replace(args.key, '***').strip()
+    print(f'error: failed to upload:\n{textwrap.indent(msg, " " * 7)}')
+    sys.exit(1)
+
+if __name__ == '__main__':
+  args = parse_arguments()
+
+  validate_arguments(args)
+  upload_strings(args, get_strings(args))
